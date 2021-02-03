02d92327effca14e6abce20ed6922bdef8732746
sune <sune1309@edu.easj.dk>
2021-02-03 21:23:13 +0100

    first commit

---
+++ Fuzz.java
@@ -0,0 +1,16 @@
+public class Fuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ fubb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {1..100}
+do
+    if ! (($i % 15));
+    then
+        echo fizzbuzz
+    elif ! (($i % 3));
+    then 
+        echo fizz
+    elif ! (($i % 5));
+    then
+        echo buzz
+    else
+        echo $i
+    fi
+done
